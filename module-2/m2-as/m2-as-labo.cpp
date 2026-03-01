c6ed816f5a1dd90ca95528157a0e30709382653c
Michal Polka <michal@michalpolka.me>
2026-03-01 13:30:32 +0100

    completed first assignment

--- module-2/m2-as/m2-as-labo.cpp
+++ module-2/m2-as/m2-as-labo.cpp
@@ -8,6 +8,42 @@ class Souris
     Compléter le code à partir d'ici
   *******************************************************/
 
+  public:
+    Souris(const double p, const string& c, const unsigned a = 0, const unsigned ev = 36)
+      : poids(p), couleur(c), age(a), esperance_vie(ev), clonee(false) {
+      cout << "Une nouvelle souris !" << endl;
+    }
+
+    Souris(Souris const& other)
+      : poids(other.poids), couleur(other.couleur), age(other.age), esperance_vie((other.esperance_vie*4)/5), clonee(true){
+      cout << "Clonage d'une souris !" << endl;
+    }
+
+    ~Souris() {
+      cout << "Fin d'une souris..." << endl;
+    }
+
+
+    void afficher() const {
+      cout << "Une souris " << couleur << (clonee? ", clonee, " : " ") << "de " << age << " mois"
+      " et pesant " << poids << " grammes" << endl;
+    }
+    void vieillir() {
+      ++age;
+      if (clonee && age>(esperance_vie/2)) couleur = "verte";
+    }
+    void evolue() {
+      age = esperance_vie-1;
+      vieillir();
+    }
+
+  private:
+    double poids;
+    string couleur;
+    unsigned age = 0;
+    unsigned esperance_vie = 36;
+    bool clonee;
+
   /*******************************************
    * Ne rien modifier après cette ligne.
    *******************************************/
